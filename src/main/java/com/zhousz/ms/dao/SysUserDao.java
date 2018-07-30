32a14a57bd40b2cff4bb850db701a43b86ea6600
zhousz <13687646803@163.com>
2018-07-30 17:35:45 +0800

    1. 初始提交 dao层 @Mapper注解可用, 后不可用. 2. 后 添加 @Component注解可用

--- src/main/java/com/zhousz/ms/dao/SysUserDao.java
+++ src/main/java/com/zhousz/ms/dao/SysUserDao.java
@@ -3,10 +3,12 @@ package com.zhousz.ms.dao;
 import com.zhousz.ms.domain.SysUser;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Mapper
+@Component
 public interface SysUserDao {
 
     @Select("select * from sys_user")
